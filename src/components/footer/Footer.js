65ecaed58393ee62e30015cb78063eb993dbf4a4
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-03-03 23:48:17 +0530

    Add footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer class="footer">
+        <p>&copy; 2025 Employee Management System | All Rights Reserved</p>
+      </footer>
+    </div>
+  );
+}
--- src/pages/Hoome.js
+++ src/pages/Hoome.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../components/nav-bar/NavBar'
+import Footer from '../components/footer/Footer'
 
 export default function Hoome() {
   return (
     <div>
-        <NavBar />
+        <NavBar/>
+        <Footer/>
     </div>
   )
 }
