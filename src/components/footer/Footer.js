c1554175000ebc0e853cbb7d29448c274aaf266c
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-03-03 23:53:50 +0530

    Update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,10 +2,16 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <footer class="footer">
-        <p>&copy; 2025 Employee Management System | All Rights Reserved</p>
+
+    <div class="d-flex flex-column min-vh-100">
+      <main class="flex-grow-1">
+      </main>
+      <footer class="bg-dark text-white text-center py-3">
+        <p class="mb-0">
+          &copy; 2025 <strong>Employee Management System</strong> | All Rights Reserved
+        </p>
       </footer>
     </div>
+    
   );
 }
